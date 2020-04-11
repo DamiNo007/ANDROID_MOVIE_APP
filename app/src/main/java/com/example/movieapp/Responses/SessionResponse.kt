36901aaf2a14296e455379af8bb7b5b73d6bec50
DamiNo007 <dmoldabayev.99@gmail.com>
2020-04-11 15:49:42 +0600

    first master commit

--- app/src/main/java/com/example/movieapp/Responses/SessionResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.movieapp.Responses
-
-import com.google.gson.annotations.SerializedName
-
-data class SessionResponse(
-    @SerializedName("success") val isSuccess:Boolean?=null,
-    @SerializedName("session_id") val session_id:String?=null
-)
--- app/src/main/java/com/example/movieapp/Sotries.kt
+++
@@ -1,9 +0,0 @@
-package com.example.movieapp
-
-import com.example.movieapp.Responses.Story
-
-class Stories {
-    companion object {
-        var stories = ArrayList<Story>(5)
-    }
-}
